96be003d4b516f2636d8735e6cd6637eaed46358
Simpson Chacko <simpson454175@gmail.com>
2018-02-20 22:34:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
-console.log('Loaded!');
+var button=doucument.getElementById("counter");
+var counter=0;
+button.onClick=function(){
+    
+    
+    
+    var span=doucument.getElementById('count');
+    counter+=1;
+    span.innerHTML=counter.toString();
+};
