90d6695208a53399d1a3ab6159f810bf09edc3de
Simpson Chacko <simpson454175@gmail.com>
2018-02-21 00:20:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 var button= document.getElementById('counter');
+var counter=0;
 button.onClick=function(){
     
-    var request = new XMLHttpRequest();
+    /*var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readystate === XMLHttpRequest.DONE){
             if(request.state === 200){
@@ -12,5 +13,8 @@ button.onClick=function(){
         }
     }
     request.open('GET','http://imad.hasura.io/counter',true);
-    request.send(null);
+    request.send(null);*/
+    var span = document.getElementById('count');
+    counter += 1;
+    span.innerHTML = counter.toString();
 };
