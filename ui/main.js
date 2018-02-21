582899bbc327dafce3531d60064ee237e8378984
Simpson Chacko <simpson454175@gmail.com>
2018-02-21 17:30:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,16 @@ var counter = 0;
 button.onclick = function () {
     counter += 1;
     span.innerHTML = counter.toString();   
-}
+};
+var submit = document.getElementById('submit_btn');
+var names = document.getElementById('name');
+submit.onclick = function () {
+    
+    var names = ['simpson', 'grindelwald', 'phoenix'];
+    var list='';
+    for(var i=0; i<names.length; i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ul = document.getElementById('list');
+    ul.innerHTML = list;
+};
